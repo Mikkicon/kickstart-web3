c2fc2b489d4f1b64d62d7df248913a7f6d26d514
Mikkicon <mikkicon@gmail.com>
2022-07-05 17:41:28 +0300

    Add ethereum and web3 type declaration to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,12 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "@mui/material";
 import darkTheme from "../styles/theme/darkTheme";
+declare global {
+  interface Window {
+    ethereum: any;
+    web3: any;
+  }  
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
